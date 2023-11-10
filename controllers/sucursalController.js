0660ab0e5140dc4a65fb82397969c1e60e148804
Manuel Gaona <81531@alumnos.utleon.edu.mx>
2023-11-09 18:38:23 -0600

    funcionalidadPrincipalSucursal

--- controllers/sucursalController.js
+++ controllers/sucursalController.js
@@ -1,10 +1,88 @@
 //importar clases
 import includesModel from "../models/includesModel.js";
+import empleadosModel from "../models/empleadosModel.js";
 
 //instanciar clases
 const includes = new includesModel();
+const empleados = new empleadosModel();
 
 // Llama a la función para incluir el header y el footer
 includes.incluirHeaderSucursal();
-
 includes.incluirFooterSucursal();
+
+//Llamar datos sessionStorege
+const usuario = sessionStorage.getItem("usuario");
+const rol = sessionStorage.getItem("rol");
+
+//Acortar funciones
+function docID(id){
+    return document.getElementById(id);
+}
+
+//cargar datos empleado 
+async function cargarDatosEmpleado(){
+        const empleado = await empleados.getDatosEmpleado(usuario, await empleados.cargarDatosEmpleados());
+        //console.log(empleado);
+        //datos de persona
+        const nombreCompleto = empleado.datosPersona.nombre + " " + empleado.datosPersona.apellidoP + " " + empleado.datosPersona.apellidoM;
+        docID("txtnombre").value = nombreCompleto;
+        docID("txtgenero").value = empleado.datosPersona.genero;
+        docID("txtfechaNac").value = empleado.datosPersona.fechaNacimiento;
+        docID("txtrfc").value = empleado.datosPersona.rfc;
+        docID("txtcurp").value = empleado.datosPersona.curp;
+        docID("txtdomicilio").value = empleado.datosPersona.datosDomicilio.domicilio;
+        docID("txtcp").value = empleado.datosPersona.datosDomicilio.cp;
+        docID("txtciudad").value = empleado.datosPersona.datosDomicilio.ciudad;
+        docID("txtestado").value = empleado.datosPersona.datosDomicilio.estado;
+        docID("txttelefono").value = empleado.datosPersona.telefono;
+        //datos laborales
+        docID("txtsucursal").value = empleado.datosLaborales.sucursal;
+        docID("txtcodigoEmpleado").value = empleado.datosLaborales.codigoEmpleado;
+        docID("txtemail").value = empleado.datosLaborales.email;
+        docID("txtpuesto").value = empleado.datosLaborales.puesto;
+        docID("txtsalario").value = empleado.datosLaborales.salario;
+        docID("txtfechaIngreso").value = empleado.datosLaborales.fechaIngreso;
+        //datos usuario
+        docID("txtnombreUsuario").value = empleado.usuario.nombreUsuario;
+        docID("txtcontrasenia").value = empleado.usuario.contrasenia;
+        docID("txtrol").value = empleado.usuario.rol;
+}
+
+cargarDatosEmpleado();
+
+//funcion para cambiar contraseña
+const btnUpdatePassword = docID("btnUpdatePassword");
+const btnUpdatePasswordConfirm = docID("btnUpdatePasswordConfirm");
+const btnUpdatePasswordCancel = docID("btnUpdatePasswordCancel");
+const txtcontrasenia = docID("txtcontrasenia");
+
+btnUpdatePassword.addEventListener("click", () => {
+    docID("txtcontrasenia").disabled = false;
+    docID("txtcontrasenia").focus();
+    docID("txtcontrasenia").select();
+    docID("txtcontrasenia").value = "";
+    btnUpdatePasswordConfirm.disabled = false;
+    btnUpdatePasswordCancel.disabled = false;
+    btnUpdatePassword.disabled = true;
+});
+
+btnUpdatePasswordConfirm.addEventListener("click", () => {
+    if(txtcontrasenia.value == ""){
+        alert("La contraseña no puede estar vacía.");
+    }
+    else{
+        empleados.updatePassword(usuario, txtcontrasenia.value);
+        docID("txtcontrasenia").disabled = true;
+        btnUpdatePasswordConfirm.disabled = true;
+        btnUpdatePasswordCancel.disabled = true;
+        btnUpdatePassword.disabled = false;
+    }
+});
+
+btnUpdatePasswordCancel.addEventListener("click", () => {
+    docID("txtcontrasenia").disabled = true;
+    btnUpdatePasswordConfirm.disabled = true;
+    btnUpdatePasswordCancel.disabled = true;
+    btnUpdatePassword.disabled = false;
+    cargarDatosEmpleado(); 
+});
